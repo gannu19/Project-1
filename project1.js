975a3746ca1f76962430b65041be21fd414a9563
gannu19 <bhukyaganapathi00@gmail.com>
2026-02-07 10:14:56 +0530

    Version 3

--- project1.js
+++ project1.js
@@ -1,1 +1,12 @@
-console.log("version 1");
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    res.json({
+        name: "Bhukya ganapathi",
+        college : "IIT KGP",
+        Roll_Number : "22MT30011"
+    })
+})
